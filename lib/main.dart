6eeb7426f57fded8ae3672e3ca1e8bed0da6f42e
Bruno Campos <bcneves13@gmail.com>
2019-10-14 21:14:01 -0300

    firt commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:todo/pages/login.page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  //This widget is the root
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
